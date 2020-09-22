923d9633af1a217f63c65a12cce3d44e719afe38
Fajariadi <fajariadi.19@gmail.com>
2020-09-22 08:50:36 +0700

    edit server.js

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use('/graphql', graphqlHTTP({
 	graphiql: true
 }));
 
-app.use(express.static('public'));
+app.use(express.static('build'));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
