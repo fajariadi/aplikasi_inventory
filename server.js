33053ea08dcdf72c9262202af627478eeb66a4ad
Fajariadi <fajariadi.19@gmail.com>
2020-09-22 09:07:57 +0700

    edit package.json

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { graphqlHTTP }= require('express-graphql');
 const schema = require('./schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -18,10 +19,10 @@ app.use('/graphql', graphqlHTTP({
 	graphiql: true
 }));
 
-app.use(express.static('build'));
+app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
