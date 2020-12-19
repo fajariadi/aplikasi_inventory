51cc13a55b0fe7703fc08df340758f9433355d80
Fajariadi <fajariadi.19@gmail.com>
2020-12-20 06:40:45 +0700

    edit dashboard

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -4,14 +4,6 @@ import { Redirect } from 'react-router-dom';
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
-
-    this.state = {
-      dropdownOpen: false,
-      radioSelected: 2,
-    };
       
     this.state = {
       nama: localStorage.getItem("nama"),
