e69d3fd14c824f6fed3f4a6a52d93a3ebe19d764
Fajariadi <fajariadi.19@gmail.com>
2021-03-03 05:38:01 +0700

    new build 3/3/2021

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b7f1470d7727368a1cf286c2e46e6a89.js"
+  "/precache-manifest.7456d7761e8e24700ddcc29b283a8740.js"
 );
 
 self.addEventListener('message', (event) => {
